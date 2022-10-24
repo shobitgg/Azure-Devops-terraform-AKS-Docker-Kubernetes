c1bff1d4eef4e7533e27bd0f3b6db6893dc5158c
SHOBIT G <41900814+shobitgg@users.noreply.github.com>
2022-10-25 00:59:22 +0530

    Create backend.sh

---
+++ terraform/backend.sh
@@ -0,0 +1,6 @@
+backend "azurerm" {
+    resource_group_name   = "jonnychipz-infra"
+    storage_account_name  = "jonnychipztstate"
+    container_name        = "tstate"
+    key                   = "77Q4LUB5o9wRdbPYDt+0kGZP+L8Sj9E/FNXg7lZBQS5z3mLod5cyan4wA19CR1SmlqIRUFQfhuQrPVaGzNhjGw=="
+}
